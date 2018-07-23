c5ea8b4927ffe5ed010946df23dc7d8e4b491170
hellolying <hellolymars@gmail.com>
2018-07-24 07:28:12 +0900

    Revert "初版一时提交"
    This reverts commit fb43a3beb8fa5c6e494c02830722f7b240a9b8ca.

--- quasar.conf.js
+++ quasar.conf.js
@@ -41,7 +41,6 @@ module.exports = function (ctx) {
     },
     // framework: 'all' --- includes everything; for dev only!
     framework: {
-      all: true,
       components: [
         'QLayout',
         'QLayoutHeader',
@@ -56,18 +55,7 @@ module.exports = function (ctx) {
         'QListHeader',
         'QItem',
         'QItemMain',
-        'QItemSide',
-        'QTabs',
-        'QTab',
-        'QTabPane',
-        'QRouteTab',
-        'QCard',
-        'QCardTitle',
-        'QCardMain',
-        'QCardMedia',
-        'QCardSeparator',
-        'QCardActions',
-        'QModal'
+        'QItemSide'
       ],
       directives: [
         'Ripple'
